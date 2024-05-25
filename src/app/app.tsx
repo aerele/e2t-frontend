aa7e4e69004e5f7c3ded803e6396465f7aee2827
l0gesh29 <logeshperiyasamy24@gmail.com>
2024-05-25 17:46:51 +0530

    feat: mobile no added in register page

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -15,7 +15,7 @@ const MyApp = ({ children }: { children: React.ReactNode }) => {
 
 	return (
 		<>
-			<FrappeProvider url={"http://localhost:8000"} socketPort={"9000"}>
+			<FrappeProvider url='http://localhost:8023' socketPort="9023">
 				<AppRouterCacheProvider options={{ enableCssLayer: true }}>
 					<ThemeProvider theme={theme}>
 						<RTL direction={customizer.activeDir}>
